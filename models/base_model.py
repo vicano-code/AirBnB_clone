0e0f3c07f79d4c134bce694f2c2b9bc968d40628
Victor <vc.anokwuru@gmail.com>
2024-03-06 07:00:57 -0800

    add new code file for data storage/other update

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ Module - BaseModel
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -17,35 +18,40 @@ class BaseModel:
         - created_at: datetime
         - updated_at: datetime
     Methods:
-        - def __init__(self, id=None, created_at=None, updated_at=None)
-        - def __str__(self)
-        - def save(self)
-        - def to_dict(self)
+        - __init__(self, id=None, created_at=None, updated_at=None)
+        - __str__(self)
+        - save(self)
+        - to_dict(self)
 
     """
 
-    def __init__(self, id=None, created_at=None, updated_at=None):
+    def __init__(self, *args, **kwargs):
         """initialize atributes"""
-        if id is None:
+        if not kwargs:
             self.id = str(uuid.uuid4())
-        else:
-            self.id = id
-        if created_at is None:
             self.created_at = datetime.now()
-        else:
-            self.created_at = created_at
-        if updated_at is None:
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
-            self.updated_at = updated_at
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        '''parse the string back into a datetime'''
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+
 
     def __str__(self):
         """override the in-built str method"""
         return "[{:s}] ({:s}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__)
+    
     def save(self):
         """updates the  attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
+
     def to_dict(self):
         """
         returns a dictionary containing all keys/values of
---
+++ models/engine/file_storage.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""
+Module - FileStorage
+
+A class for serialization and deserialization
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    serializes instances to a JSON file and
+    deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            if (type(obj) == dict):
+                obj_dict[key] = obj
+            else:
+                obj_dict[key] = obj.to_dict()
+        with open(self.__file_path, mode="w", encoding="utf-8") as json_file:
+            json.dump(obj_dict, json_file)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects (only if the
+        JSON file (__file_path) exists ; otherwise, do nothing)
+        """
+        try:
+            with open(self.__file_path, "r") as json_file:
+                json_obj = json.load(json_file)
+            for key, obj in json_obj.items():
+                self.__objects[key] = obj
+        except FileNotFoundError:
+            pass

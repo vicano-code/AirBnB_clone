2dcbcbe872c08b098918440ec4465c8320ec9cfc
Victor <vc.anokwuru@gmail.com>
2024-03-09 13:26:52 -0800

    correction check

--- console.py
+++ console.py
@@ -166,7 +166,4 @@ class HBNBCommand(cmd.Cmd):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        HBNBCommand().onecmd(' '.join(sys.argv[1:]))
-    else:
-        HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()

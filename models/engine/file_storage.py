a64a1e63737ead117b5eb3bed995a0acd1ac46fb
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2024-03-11 06:21:26 -0700

    push test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,15 +55,8 @@ class FileStorage:
         try:
             with open(self.__file_path, "r") as json_file:
                 json_obj = json.load(json_file)
-<<<<<<< HEAD
-            for key, obj_data in json_obj.items():
-                class_name = obj_data["__class__"]
-                del obj_data["__class__"]
-                obj = self.class_dic[class_name](**obj_data)
-=======
             for key, obj in json_obj.items():
                 obj = self.class_dic[obj['__class__']](**obj)
->>>>>>> 23d694ef77277f8b233ce32f16e29c557f4d6dbe
                 self.__objects[key] = obj
         except FileNotFoundError:
             pass
